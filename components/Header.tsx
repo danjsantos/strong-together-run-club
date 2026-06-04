bcd547674f047b90b6b7747acf0f160d51a895d7
Daniel Santos <38601273+danjsantos@users.noreply.github.com>
2026-06-04 13:13:33 -0400

    Add files via upload

--- components/Header.tsx
+++ components/Header.tsx
@@ -59,9 +59,9 @@ export default function Header() {
       }`}
     >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center justify-between h-20"> {/* era h-16, aumentei para h-20 */}
           <Link href="/" className="flex-shrink-0" onClick={() => setIsMenuOpen(false)}>
-            <Logo className="h-9 w-auto" />
+            <Logo className="h-16 w-auto" /> {/* era h-9, aumentei para h-16 */}
           </Link>
 
           {/* Desktop nav */}
